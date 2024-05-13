ac6e8667f010bd7572c34def92fb6ab075c945a2
Lucas Mendonça da Silva Junior <lucasjr413@gmail.com>
2024-05-12 23:56:12 -0300

    CAPITULO 1 - Busca Binaria

---
+++ Cap1_PesquisaBinaria/Exemplo/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exemplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = { 1, 3, 5, 7, 9};
+
+            Console.WriteLine(BuscaBinaria(values, 9)); 
+            Console.WriteLine(BuscaBinaria(values, -1)); 
+        }
+
+        static int BuscaBinaria(int[] lista, int item)
+        {
+            int menor = 0; 
+            int maior = lista.Length -1;
+
+            while (menor <= maior)
+            {
+                int mid = (menor + maior) / 2; 
+                int chute = lista[mid];
+
+                if(chute == item) return mid;
+
+                if (chute > item) maior = mid - 1;
+
+                else
+                {
+                    menor = mid + 1;
+                }
+            }
+
+            return -1; 
+        }
+
+    }
+}
