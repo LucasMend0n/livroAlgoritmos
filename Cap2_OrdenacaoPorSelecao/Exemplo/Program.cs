0989ba4bd82cbf32bba63416acd42302cd5e16a5
Lucas Mendonça da Silva Junior <lucasjr413@gmail.com>
2025-01-09 01:35:51 -0300

    add - old exercises

---
+++ Cap2_OrdenacaoPorSelecao/Exemplo/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Exemplo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayteste = new int[] { 5, 3, 6, 2, 10 };
+
+            Console.WriteLine(OrdenacaoPorSelecao(arrayteste)); 
+        }
+
+        static int BuscaMenor(int[] array)
+        {
+            int menor = array[0];
+            int menorIndice = 0; 
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < menor)
+                {
+                    menor = array[i];
+                    menorIndice = i;
+                } 
+            }
+            return menorIndice;
+        }
+
+        static int[] OrdenacaoPorSelecao(int[] array)
+        {
+             var newArr = new int[] { };
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                int menor = BuscaMenor(array);
+                newArr.Append(array[i]); 
+            }
+            return newArr;
+        }
+    }
+}
---
+++ Cap4_Quicksort/Exemplo/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+ static int soma(int[] lista)
+{
+    if(lista == null || lista.Length == 0)
+    {
+        return 0;
+    }
+
+    return lista[0] + soma(lista);
+}
+
+Console.WriteLine(soma([1, 5, 4]));
